aab2a2771da87066700816484c27ff68c78e60ab
louispallett <louis.pallett@outlook.com>
2023-06-08 13:37:12 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let myLibrary = [];
+
+function Book(title, author) {
+    this.title = title;
+    this.author = author;
+}
+
+function addBookToLibrary() {
+    const userBook = prompt("Add a book to the library", "War and Peace");
+}
+
+//addBookToLibrary();
