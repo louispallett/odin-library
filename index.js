57a18436bc8c3713cc531c367262b0f2a9e5a156
louispallett <louis.pallett@outlook.com>
2023-06-09 23:45:39 +0100

    Added constructor and addToLibrary() to add new Books to myLibrary[]

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
+//let newBook = document.getElementById("books");
 let myLibrary = [];
 
 function Book(title, author) {
     this.title = title;
     this.author = author;
+
+    this.addToLibrary();
 }
 
-function addBookToLibrary() {
-    const userBook = prompt("Add a book to the library", "War and Peace");
+Book.prototype.addToLibrary = function() {
+    myLibrary.push(this);
 }
 
 //addBookToLibrary();
+
+const warAndPeace = new Book ("War and Peace", "Leo Tolstoy");
+const dune = new Book ("Dune", "Frank Herbert");
+const theAeneid = new Book("The Aeneid", "Virgil");
+
+console.log(myLibrary);
+console.log(Object.getPrototypeOf(Book));
+
+/* for(let i = 0; i < myLibrary.length; i++) {
+    newBook.textContent = myLibrary[i];
+} */
